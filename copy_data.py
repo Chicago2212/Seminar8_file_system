ef3e5297b49df4f8ef42f97f81bf6215b8c30a9f
Nastassia <anastasia.goroninova@icloud.com>
2023-10-09 21:59:54 +0300

    change file

--- copy_data.py
+++ copy_data.py
@@ -19,10 +19,13 @@ def copy_row():
     surname = data[2]
     birthdate = data[3]
     town = data[4]
-    with open(f'db/data_{nf_2}.txt', 'r', encoding='utf-8') as file:
+    with open(f'Seminar8_file_system/db/data_{nf_2}.txt', 'r', encoding='utf-8') as file:
         data_2 = file.readlines()
     now_number_row = len(data_2) + 1
-    with open(f'db/data_{nf_2}.txt', 'a', encoding='utf-8') as file:
+    with open(f'Seminar8_file_system/db/data_{nf_2}.txt', 'a', encoding='utf-8') as file:
         file.write(f'{now_number_row};{name};'
                    f'{surname};{birthdate};{town}')
     print("Данные успешно скопированы!")
+
+    
+
