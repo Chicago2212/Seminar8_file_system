6624284e1c57064590e66dc850b141064fde415c
Okonoki <okonokiss@gmail.com>
2023-10-09 23:15:31 +0500

    task done

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from ui import start_menu
 
-
 if __name__ == '__main__':
     start_menu()
--- return_data_file.py
+++ return_data_file.py
@@ -1,7 +1,7 @@
 from choice_file import choice_number_file
 
 
-def data_file():
+def data_file():  # открывает для чтения файл по выбранному номеру nf
     nf = choice_number_file()
     with open(f'db/data_{nf}.txt', 'r', encoding='utf-8') as file:
         data = file.readlines()
