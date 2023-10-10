0a0e1004c29fda9e7ad6e4acf3c9289128528a4f
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-10-10 16:29:10 +0300

    final commit

--- copy_1.py
+++ copy_1.py
@@ -6,12 +6,18 @@ def copy_file():
     data_from, n_copy = data_file_copy_from()
     data_to, n_to = data_file_copy_to()
     count_rows_from = len(data_from)
+    count_rows_to = len(data_to)
 
     if count_rows_from == 0:
         print("Файл пустой!")
     else:
         number_row_from = int(input(f"Введите номер копируемой строки "
                                     f"от 1 до {count_rows_from}: "))
-
-        data_to_copy = data_from[number_row_from - 1]
-        print(data_to_copy)
+        while (number_row_from > count_rows_from):
+            number_row_from = int(input(f"Введите номер копируемой строки "
+                                        f"от 1 до {count_rows_from}: "))
+            
+        data_to_write = f"{count_rows_to + 1}" + data_from[number_row_from - 1][1:]
+        with open(f'db/data_{n_to}.txt', 'a', encoding='utf-8') as file:
+            file.write(data_to_write)
+        print("Данные успешно скопированы!\n")
