f796e84a406c120ec09645cd7eb107aeaf59d005
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-10-10 19:10:03 +0300

    Final_changes

--- copy_1.py
+++ copy_1.py
@@ -16,8 +16,9 @@ def copy_file():
         while (number_row_from > count_rows_from):
             number_row_from = int(input(f"Введите номер копируемой строки "
                                         f"от 1 до {count_rows_from}: "))
-            
-        data_to_write = f"{count_rows_to + 1}" + data_from[number_row_from - 1][1:]
+
+        data_to_write = f"{count_rows_to + 1}" + \
+            data_from[number_row_from - 1][1:]
         with open(f'db/data_{n_to}.txt', 'a', encoding='utf-8') as file:
             file.write(data_to_write)
         print("Данные успешно скопированы!\n")
