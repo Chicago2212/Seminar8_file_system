b4f44740c6402c5587115367c7c7266436047cde
Юрий <138233506+3amnotex@users.noreply.github.com>
2023-10-11 14:05:42 +0300

    Add files via upload
    Добавлен файл копирования

--- choice_file.py
+++ choice_file.py
@@ -3,7 +3,7 @@ from print_data import print_file
 
 def choice_number_file():
     print_file()
-    number = int(input("Выберит файл, с которым Вы хотите работать\n"
+    number = int(input("Выберите файл, с которым Вы хотите работать\n"
                        "Введите цифру 1 или 2: "))
     while number < 1 or number > 2:
         number = int(input("Ошибка!!!\n"
