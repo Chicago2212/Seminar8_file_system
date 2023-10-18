1963419fbd0daa83f2eb3c4c52efd894879c377e
alexeicaps <caps.alex@mail.ru>
2023-10-18 14:48:18 +0300

    resolved copy_file.py

--- copy_file.py
+++ copy_file.py
@@ -1,32 +1,23 @@
 from choice_file import choice_number_file
 from return_data_file import data_file
-from print_data import print_file
 
 
 def copy_data():
-    print_file()
     source_nf = int(input("Какой файл будем копировать, 1 или 2: "))
     with open(f'db/data_{source_nf}.txt', 'r', encoding='utf-8') as file:
         source_data = file.readlines()
-        print(source_data)
 
     destination_nf = int(input("Выберите в какой файл будем вставлять данные: "))
-    # with open(f'db/data_{destination_nf}.txt', 'r', encoding='utf-8') as file:
-    #     destination_data = file.readlines()
-        print(destination_data)
 
     while destination_nf == source_nf:
         destination_nf = int(input("Ошибка!\n"
-                      "Выберите файл, который не совпадает с копируемым."
-                      "Выберите в какой файл будем вставлять данные: "))
-
-    # data = data_file()
-    # now_number_row = len(data) + 1
+                                   "Выберите файл, который не совпадает с копируемым."
+                                   "Выберите в какой файл будем вставлять данные: "))
 
     with open(f"db/data_{destination_nf}.txt", 'a', encoding="utf-8") as file:
         file.writelines(source_data)
-    print("Данные успешно скопированы!")
-    # print(source_data)
+        print("Данные успешно скопированы!")
+        print()
 
 
 
@@ -39,28 +30,7 @@ def copy_data():
 
 
 
-# from print_data import print_file
-# # from choice_file import choice_number_file
-# def copy_data():
-#     print("Выберите файл-источник:")
-#     source_nf = print_file()
-#     with open(f'db/data_{source_nf}.txt', 'r', encoding='utf-8') as file:
-#         source_data = file.readlines()
-#
-#     print("Выберите файл-приемник:")
-#     target_nf = print_file()
-#     while target_nf == source_nf:  # Убеждаемся, что файлы не одинаковые
-#         print("Файл-приемник не должен совпадать с файлом-источником. Пожалуйста, выберите другой файл.")
-#         target_nf = print_file()
-#
-#     with open(f'db/data_{target_nf}.txt', 'a', encoding='utf-8') as file:
-#         file.writelines(source_data)
-#
-#     print(f"Данные из файла {source_nf} были успешно скопированы в файл {target_nf}!")
 
-
-
-#
 # from choice_file import choice_number_file
 # def copy_data():
 #     print("Выберите файл-источник:")
