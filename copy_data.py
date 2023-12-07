cb6412e9c02f2011f53a0308f318f9c9b8e55906
Irada <iguluzada@rambler.ru>
2023-12-07 19:50:21 +0400

    Copy_data

---
+++ copy_data.py
@@ -0,0 +1,7 @@
+from return_data_file import data_file
+
+
+def copy_row():
+   data, nf = data_file()
+   count_rows = len(data)
+     
--- ui.py
+++ ui.py
@@ -2,10 +2,10 @@ from delete_data import delete_row
 from change_data import change_row
 from add_data import add_row
 from print_data import print_file
-
+from copy_data import copy_row
 
 def check_number(n):
-    while n < 1 or n > 5:
+    while n < 1 or n > 6:
         n = int(input("Ошибка, такого номера команды не "
                       "существует! Введите цифру от 1 до 5\n"
                       "Выберите функцию:\n"
@@ -13,21 +13,23 @@ def check_number(n):
                       "2. Удалить\n"
                       "3. Изменить\n"
                       "4. Вывод\n"
-                      "5. Выход\n"
+                      "5. Копирование\n"
+                      "6. Выход\n"
                       "Введите номер команды: "))
     return n
 
 
 def start_menu():
     command = None
-    while command != 5:
+    while command != 6:
         command = int(input("Доброго времени суток!\n"
                             "Выберите функцию:\n"
                             "1. Добавить\n"
                             "2. Удалить\n"
                             "3. Изменить\n"
                             "4. Вывод\n"
-                            "5. Выход\n"
+                            "5. Копирование\n"
+                            "6. Выход\n"
                             "Введите номер команды: "))
         command = check_number(command)
         if command == 1:
@@ -38,5 +40,7 @@ def start_menu():
             change_row()
         elif command == 4:
             print_file()
+        elif command == 5:
+            copy_row()
     print("Спасибо, что воспользовались нашими услугами!\n"
           "Всего доброго! Приходите к нам ещё :)")
